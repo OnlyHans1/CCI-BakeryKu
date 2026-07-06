4f3bed4fe23b81301e243efcb24ce15d81f077f2
OnlyHans1 <adityarhaihan@gmail.com>
2026-07-06 19:39:39 +0700

    initial commit

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+
+import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+import { Table } from "@/components/table"
+import { Skeleton } from "@/components/skeleton"
+
+
+export default function ProductPage() {
+    return (
+        <div>
+            <h1>Product Page</h1>
+        </div>
+    )
+}
